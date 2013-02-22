1a187be0ac36f122d90c8a0e5ec2edd4f1432ca3
LimeBlast <daniel@ghostds.com>
2013-02-22 12:00:27 +0000

    Made a start on getting this all together, but something still doesn't work correctly

--- sa/source/templates/footer.php
+++ sa/source/templates/footer.php
@@ -1,9 +1,22 @@
-	<div id="footer">
-		<p>
-			<a href="http://wordpress.org/" rel="generator">Proudly powered by WordPress</a>
-		</p>
-	</div><!--end footer-->
-</div><!--end container-->
+	<footer role="contentinfo" class="footer">
+
+		<div class="footer-telephone">
+			<div class="wrap">
+				tel: 01926 450 391
+			</div>
+		</div><!-- .footer-telephone -->
+
+		<div class="wrap">
+			<div class="footer-copyright">
+				Designed by Ghost Design
+			</div><!-- .footer-copyright -->
+
+			<div class="footer-accreditations">
+				[logo here] [logo here]
+			</div><!-- .footer-accreditations -->
+		</div><!-- .wrap -->
+
+	</footer><!-- .footer -->
 <?php wp_footer(); ?>
 </body>
 </html>
--- sa/source/templates/header.php
+++ sa/source/templates/header.php
@@ -1,29 +1,55 @@
-<html <?php language_attributes( 'html' ) ?>>
+<!DOCTYPE html>
+<!--[if lt IE 7]>  <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes( 'html' ) ?>> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" <?php language_attributes( 'html' ) ?>> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" <?php language_attributes( 'html' ) ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes( 'html' ) ?>> <!--<![endif]-->
 <head>
 	<title><?php wp_title(); ?></title>
 	<!-- Basic Meta Data -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+	<meta name="viewport" content="width=device-width" />
+	<link rel="author" href="/humans.txt" />
 
 	<!-- WordPress -->
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div id="header" class="clear">
-		<div id="title">
-			<?php if ( is_front_page() ) echo( '<h1>' ); ?>
-				<a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
-			<?php if ( is_front_page() ) echo( '</h1>' ); ?>
-		</div>
-		<?php
-			wp_nav_menu(
-				array(
-					'theme_location'  => 'nav-1',
-					'container_id'    => 'navigation',
-					'container_class' => 'clear',
-					'menu_class'      => 'nav',
-					'depth'           => '2'
-					)
-				);
-		?>
-	</div><!--end header-->
+
+	<!--[if lt IE 7]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+	<![endif]-->
+
+	<div class="background">
+
+		<header role="banner" class="header">
+			<div class="wrap">
+
+				<div class="header-title">
+					<a href="<?php echo home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/header-logo.png" alt="<?php bloginfo( 'name' ); ?>" title="<?php bloginfo( 'name' ); ?>" /></a>
+				</div><!-- .header-title -->
+
+				<nav role="navigation" class="header-navigation">
+					<ul class="header-links">
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">News</a></li>
+					</ul><!-- .header-links -->
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location'  => 'nav-1',
+							'container'       => false,
+							'menu_class'      => 'nav',
+							'depth'           => '1'
+						)
+					);
+					?>
+				</nav><!-- .header-navigation -->
+
+			</div><!-- .wrap -->
+		</header><!-- .header -->
+
+	</div><!-- .background-->
