23366bb952743b418cdb40d1be0968d2a9b76ad9
LimeBlast <daniel@ghostds.com>
2013-02-25 14:47:35 +0000

    Removed multiple content blocks, replaced with advanced custom fields and custom post types ui

--- sa/source/templates/page.php
+++ sa/source/templates/page.php
@@ -9,7 +9,7 @@
 				<?php if (!is_front_page()): ?>
 					<h1><?php the_title(); ?></h1>
 				<? endif; ?>
-				<?php the_block('Standout') ?>
+				<?php the_field('content'); ?>
 
 			</div><!-- .wrap -->
 		</div><!-- .main-standout -->
