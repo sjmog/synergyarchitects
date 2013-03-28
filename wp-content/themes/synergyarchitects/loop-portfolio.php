ad7aa928dd80f86ffa983a25aa61bd780ba64ac8
LimeBlast <daniel@ghostds.com>
2013-03-28 12:12:07 +0000

    I'm unable to do the 3d rotation, so I hope what I have will be a good substitute.

--- wp-content/themes/synergyarchitects/loop-portfolio.php
+++ wp-content/themes/synergyarchitects/loop-portfolio.php
@@ -4,22 +4,21 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<li>
 				<div id="post-<?php the_ID(); ?>" <?php post_class('loop-portfolio'); ?>>
-					<?php the_post_thumbnail( 'portfolio-loop' ); ?>
+					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'portfolio-loop' ); ?></a>
 					<h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-					<?php the_excerpt(); ?>
 				</div><!--end post-->
 			</li>
 		<?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
 	</ul>
 
-		<div class="pagination index">
-			<div class="alignleft">
-				<?php previous_posts_link( __( '&larr; Newer entries', 'sa' )); ?>
-			</div>
-			<div class="alignright">
-				<?php next_posts_link( __( 'Older entries &rarr;', 'sa' )); ?>
-			</div>
-		</div><!--end pagination-->
+	<div class="pagination index">
+		<div class="alignleft">
+			<?php previous_posts_link( __( '&larr; Newer entries', 'sa' )); ?>
+		</div>
+		<div class="alignright">
+			<?php next_posts_link( __( 'Older entries &rarr;', 'sa' )); ?>
+		</div>
+	</div><!--end pagination-->
 
 	<?php else : ?>
 <?php endif; ?>
