a0fbf62e03c425a6c7f600e6e1ebb2cbb43d558e
LimeBlast <daniel@ghostds.com>
2013-05-21 11:25:35 +0100

    Reet, lets get this party started

---
+++ Guardfile
@@ -0,0 +1,9 @@
+# Guardfile: https://github.com/guard/guard#readme
+
+guard 'compass', :configuration_file => 'config.rb' do
+  watch(%r{\.scss$})
+end
+
+guard 'livereload' do
+  watch(%r{\.(css|js|html|php)})
+end
---
+++ config.rb
@@ -0,0 +1,13 @@
+## http://compass-style.org/help/tutorials/configuration-reference/
+
+# compass plugins
+require 'susy' # gem install susy
+
+# compass settings
+sass_dir = 'css'
+css_dir = 'css'
+images_dir = 'img'
+javascripts_dir = 'js'
+
+# sass settings
+sass_options = {:debug_info => true}
