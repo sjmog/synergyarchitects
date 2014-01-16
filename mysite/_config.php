eff5911816b1781700ce18d4e9cf157c82889d6c
LimeBlast <daniel@ghostds.com>
2014-01-16 13:07:46 +0000

    added config file within mysite: configure this for new environments

---
+++ mysite/_config.php
@@ -0,0 +1,26 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'friendly',
+	"database" => 'synergyarchitects',
+	"path" => '',
+);
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('synergyarchitects');
+
+// Set the site locale
+i18n::set_locale('en_GB');
+
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
